332d4b87b0e2cffb13c6cc799b45caa6b2d9f965
yuxintao <1921056015@qq.com>
2024-04-07 14:20:43 +0800

    java代理模式详解

--- src/main/java/org/jingtao8a/java_guide/test1.java
+++ src/main/java/org/jingtao8a/java_guide/test1.java
@@ -9,5 +9,11 @@ public class test1 {
         ProxyUtils.JDKProxyTest();
         //CGLIB动态代理
         ProxyUtils.CGLIBProxyTest();
+        // JDK动态代理只能代理实现了接口的类或者直接代理接口
+        // CGLIB动态代理可以代理未实现任何接口的类
+        // CGLIB是实现一个被代理类的子类，因此不能代理声明为final类型的类和方法
+
+        //静态代理是在编译时就生成了一个个class文件
+        //动态代理是运行时动态生成类字节码，并加载到JVM中的
     }
 }
