21499d9317c1d40f0ba28df2dc52b6335f4d4250
yuxintao <1921056015@qq.com>
2024-09-30 14:49:59 +0800

    update ArraysKind.java

--- src/main/java/org/jingtao8a/api/ArraysKind.java
+++ src/main/java/org/jingtao8a/api/ArraysKind.java
@@ -3,12 +3,20 @@ package org.jingtao8a.api;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 public class ArraysKind {
     @Test
     public void test1() {
-        int[] arr = {2, 34, 43, 23, 24};
-        System.out.println(arr);
-        System.out.println(Arrays.toString(arr));
+        Map<String, String> map = new HashMap<>();
+        map.put("a","A");
+        map.put("b","B");
+        String v = map.computeIfAbsent("b",k->"v");  // 输出 B
+        System.out.println(v);
+        String v1 = map.computeIfAbsent("c",k->"v"); // 输出 v
+        System.out.println(v1);
     }
 }
