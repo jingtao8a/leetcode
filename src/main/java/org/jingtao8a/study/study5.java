66bd1a547e5096e9b71c85b09c21e821bb9a4ac5
yuxintao <1921056015@qq.com>
2025-02-02 17:02:38 +0800

    modify quickSort

--- src/main/java/org/jingtao8a/study/study5.java
+++ src/main/java/org/jingtao8a/study/study5.java
@@ -1,44 +1,44 @@
 package org.jingtao8a.study;
 
 public class study5 {
-    private static int partition(int[] nums, int left, int right) {
-        int mid = (left + right) >> 1;
-        swap(nums, left, mid);
-        int pivot = nums[left];
+    private static int randomInt(int min, int max) {
+        return (int)Math.random() * (max - min) + min;
+    }
+
+    private static int partition(int[] arr, int left, int right) {
+        int tmp = randomInt(left, right);
+        swap(arr, left, tmp);
+        int pivot = arr[left];
         int i = left;
-        int j = right;
-        while (i < j) {
-            while (nums[j] < pivot && i < j) {
-                --j;
-            }
-            while (nums[i] >= pivot && i < j) {
+        for (int j = left + 1; j <= right; ++j) {
+            if (arr[j] <= pivot) {
                 ++i;
-            }
-            if (i != j) {
-                swap(nums, i, j);
+                if (i != j) {
+                    swap(arr, i, j);
+                }
             }
         }
-        swap(nums, i, left);
+        swap(arr, left, i);
         return i;
     }
 
-    private static void swap(int[] nums, int left, int right) {
-        int temp = nums[left];
-        nums[left] = nums[right];
-        nums[right] = temp;
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 
-    public static void quickSort(int[] nums, int left, int right) {
+    private static void quickSort(int[] arr, int left, int right) {
         if (left >= right) {
             return;
         }
-        int mid = partition(nums, left, right);
-        quickSort(nums, left, mid - 1);
-        quickSort(nums, mid + 1, right);
+        int mid = partition(arr, left, right);
+        quickSort(arr, left, mid - 1);
+        quickSort(arr, mid + 1, right);
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] nums = new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1};
         quickSort(nums, 0, nums.length - 1);
         for (int i = 0; i < nums.length; i++) {
             System.out.println(nums[i]);
